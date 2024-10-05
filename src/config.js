6d0fbc66bfb89fd14c15f2ee51c2ab303007a155
Areesha1416 <143162257+Areesha1416@users.noreply.github.com>
2024-10-05 19:42:59 +0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -111,7 +111,7 @@ export const projectCardImages = [
   Add your formspree endpoint below.
   https://formspree.io/
 */
-export const formspreeUrl = "https://formspree.io/f/YourEndpoint";
+export const formspreeUrl = "https://formspree.io/f/manywokj";
 
 // Footer icons theme (light or dark)
 export const footerTheme = "dark";
